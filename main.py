646c2963d68b1fafc26ffbd7e12ce507a1e0414a
NotNullBool <106617184+NotNullBool@users.noreply.github.com>
2023-10-08 14:37:51 -0400

    basic slash command test

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
-import os
 import discord
+from discord import app_commands
 import json
 
 intents = discord.Intents.default()
-
 client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
+
+@tree.command(name="ping", description="ping the bot!", guild=discord.Object(id=1004855799044120699))
+async def ping(interaction: discord.Interaction):
+    await interaction.response.send_message("pong")
 
 @client.event
 async def on_ready():
+    await tree.sync(guild=discord.Object(id=1004855799044120699))
     print(f"Logged in as {client.user}")
 
-# @client.event
-# async def on_message(message):
-#     if message.author == client.user:
-#         return
-#
-    # print(f"User:{message.author} Message:{message.content}")
-
+#load token from json
 token = {"token": ""}
+
 try:
     with open("token.json","r") as fp:
         token = json.load(fp)
@@ -26,4 +26,5 @@ except Exception as e:
         json.dump(token, fp)
     print("Please provide token in token.json!")
     raise
+
 client.run(token["token"])
